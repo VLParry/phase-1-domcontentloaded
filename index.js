85a5530955782608f28d1068dcd74f130e4a9d2f
Victoria Parry <vlparry@gmail.com>
2022-05-17 13:50:07 -0400

    commit tree c7c96553416e29d8fb16b72d565dc2475d335cea

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// 
+
+let text = document.getElementById('text');
+document.addEventListener("DOMContentLoaded", (event) => {
+    text.textContent = `This is really cool!`
+})
+
+// console.log(
+//     "this console.log() fires when index.js loads- before DOMContentLoaded is triggered"
+// )
